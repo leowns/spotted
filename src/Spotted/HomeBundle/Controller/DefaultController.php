94517808f81f29271f0bb82e13e152261c04eec2
Leo <lee_ati@hotmail.com>
2013-11-09 10:15:18 +0100

    Designed Login View 2/2

--- src/Spotted/HomeBundle/Controller/DefaultController.php
+++ src/Spotted/HomeBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('SpottedHomeBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('SpottedHomeBundle:Default:index.html.twig');
+    }
+
+    public function commentAction($id)
+    {
+        return $this->render('SpottedHomeBundle:Default:comment.html.twig', array('id' => $id));
     }
 }
